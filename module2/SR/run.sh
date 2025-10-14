6bcb496da0ed1b969ebdbe5e8e1c3acc295f0231
Mihari <miharisama@foxmail.com>
2025-10-15 00:48:55 +0800

    (Update): run.sh logic and .gitignore

--- module2/SR/run.sh
+++ module2/SR/run.sh
@@ -1,6 +1,14 @@
-rm ./logs/*.txt
+# clean old logs and build
+rm -r ./logs
+rm -r ./build
+
+# cmake & build
+mkdir -p ./build
 cd ./build
 cmake ..
 make
 cd ..
+
+# run
+mkdir -p ./logs
 ./bin/stop_wait > ./logs/terminal_log.txt
